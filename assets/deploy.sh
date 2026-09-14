0d7f295b19f20963750a5b00aa627e6dec9e5778
Jodi Hunt <jodihunt@MacBook-Pro.local>
2026-09-13 18:44:30 -0600

    Update deployment script for all site assets

--- assets/deploy.sh
+++ assets/deploy.sh
@@ -4,5 +4,22 @@ set -euo pipefail
 site_root="${HOME}/public_html"
 mkdir -p "$site_root"
 
-cp ./*.html ./*.css ./*.js ./*.png ./*.jpg ./*.JPG "$site_root/"
+shopt -s nullglob
+
+root_files=(
+	./*.html
+	./*.css
+	./*.js
+	./*.png
+	./*.jpg
+	./*.JPG
+	./*.jpeg
+	./*.JPEG
+)
+
+if ((${#root_files[@]})); then
+	cp "${root_files[@]}" "$site_root/"
+fi
+
 cp -R assets "$site_root/"
+cp -R "Emplolyee Images" "$site_root/"
